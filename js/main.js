cb485b616453872a196b60593d94a11de453341b
boburweb3044 <boburwebb@gmail.com>
2024-02-12 20:52:11 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// console.log(10/2);
+
+// var a = 15.727228
+// console.log(a);
+// console.log(Math.floor(a));
+// console.log(Math.round(a));
+// console.log(Math.ceil(a));
+// console.log(a.toFixed(3));
+// console.log(Math.floor(Math.random() * 100));
+
+// var a = 10
+// var b = '10'
+// console.log(a === b);
+// console.log(a > b);
+// console.log(b > a);
+// console.log(a <= b);
+// console.log(a >= b);
+
+var a = prompt('a:')
+if(a > 18){
+    console.log('aka');
+}else if(a < 18){
+    console.log('uka');
+}else if(a == 18){
+    console.log('dost');
+}
+
+// var elTitle = document.querySelector('.title')
+// console.log(elTitle);
+// elTitle.textContent = 'salom'
+
+// var a = 10
+// var b = 10
+
+// if(a == b){
+//     elTitle.textContent = 'teng'
+// }else{
+//     elTitle.textContent = 'teng emas'
+// }
+
